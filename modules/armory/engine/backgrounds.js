03323e929a354bbe7983226705b9252adb3418b8
DarkOneMC <williammurphy812@gmail.com>
2026-07-02 17:54:56 +0000

    Add armory zone background engine

--- modules/armory/engine/backgrounds.js
+++ modules/armory/engine/backgrounds.js
@@ -5,13 +5,45 @@ function slug(value) {
     .replace(/^-+|-+$/g, "");
 }
 
+const ZONE_BACKGROUNDS = {
+  210: "icecrown",
+  4395: "dalaran",
+  1519: "stormwind",
+  1637: "orgrimmar"
+};
+
+const MAP_BACKGROUNDS = {
+  571: "northrend",
+  0: "eastern-kingdoms",
+  1: "kalimdor"
+};
+
 function getArmoryBackground(ch, helpers = {}) {
   const className = helpers.className || ((id) => String(id || "unknown"));
 
+  const zoneId = Number(ch.zone || 0);
+  const mapId = Number(ch.map || 0);
   const classKey = slug(className(ch.class));
 
+  if (ZONE_BACKGROUNDS[zoneId]) {
+    return {
+      type: "zone",
+      key: ZONE_BACKGROUNDS[zoneId],
+      url: `/images/armory/backgrounds/zones/${ZONE_BACKGROUNDS[zoneId]}.jpg`
+    };
+  }
+
+  if (MAP_BACKGROUNDS[mapId]) {
+    return {
+      type: "map",
+      key: MAP_BACKGROUNDS[mapId],
+      url: `/images/armory/backgrounds/zones/${MAP_BACKGROUNDS[mapId]}.jpg`
+    };
+  }
+
   return {
-    classKey,
+    type: "class",
+    key: classKey,
     url: `/images/armory/backgrounds/classes/${classKey}.jpg`
   };
 }
